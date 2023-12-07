3a0752597757e969b2f6db00aab0dd9c43890c03
Aminekham <lkham0508@gmail.com>
2023-12-07 20:46:44 +0100

    transpose first try

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" doc here """
+shape = __import__('2-size_me_please').matrix_shape
+
+
+
+def add_matrices2D(mat1, mat2):
+    if shape(mat1) != shape(mat2):
+        return(None)
+    matrix_result = []
+    for j in range(len(mat1)):
+        temp = []
+        for i in range(len(mat1[j])):
+            temp.append(mat1[j][i] + mat2[j][i])
+        matrix_result.append(temp)
+    return(matrix_result)
