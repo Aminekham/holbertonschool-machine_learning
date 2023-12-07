52fb84e553f39aff516215f0de00b674a8ff5fc2
Aminekham <lkham0508@gmail.com>
2023-12-07 16:53:16 +0100

    fixing the shape problem

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -2,6 +2,10 @@
 def matrix_shape(matrix):
     temp = matrix
     shape = []
+    shape.append(len(temp))
     for i in range(len(temp)):
+        if type(temp[i]) != list:
+            break
         shape.append(len(temp[i]))
+        temp = temp[i]
     return(shape)
