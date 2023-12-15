16ed98d49fa2801d300ca01ad71c8538d8bbcf57
Aminekham <lkham0508@gmail.com>
2023-12-15 22:19:07 +0100

    fixing the invalid types

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -6,14 +6,16 @@ function to get the derivative of a polynominal function
 
 def poly_derivative(poly):
     """
-    Parameters: 
+    Parameters:
         derivative: list to save the coefficents of the derivative
     """
-    derivarive = []
-    if type(poly) != list:
-        return(None)
+    if not isinstance(poly, list):
+        return None
+    if not all(isinstance(coeff, (int, float)) for coeff in poly):
+        return None
+    derivative = []
     for i in range(len(poly)):
-        derivarive.append(poly[i] * i)
-    if sum(derivarive) == 0:
-        return([0])
-    return(derivarive[1:])
+        derivative.append(poly[i] * i)
+    if sum(derivative) == 0:
+        return [0]
+    return derivative[1:]
