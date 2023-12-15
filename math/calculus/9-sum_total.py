616ecdab9db22c2d76e4918f03d8f43511d37e68
Aminekham <lkham0508@gmail.com>
2023-12-15 21:58:07 +0100

    sum in recursion

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -9,6 +9,13 @@ def summation_i_squared(n):
         sum: The variable storing the square of i
         each time
     """
+    # Check if n is a valid number
+    if not isinstance(n, int) or n < 1:
+        return None
+
+    # Base case: return 1 if n is 1
     if n == 1:
-        return(1)
-    return(n ** 2 + summation_i_squared(n - 1))
+        return 1
+
+    # Recursive case: calculate i^2 + summation_i_squared(n-1)
+    return n**2 + summation_i_squared(n-1)
