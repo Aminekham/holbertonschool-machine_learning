d34a81bf3f9315718fa23483e2e24a9c9b20d662
Aminekham <lkham0508@gmail.com>
2023-12-15 22:00:50 +0100

    numpy is the key

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -3,19 +3,16 @@
 function to get the sum of i squared
 from 1 to the given n
 """
+import numpy as np
+
+
 def summation_i_squared(n):
     """
     Parameters: 
         sum: The variable storing the square of i
         each time
     """
-    # Check if n is a valid number
     if not isinstance(n, int) or n < 1:
         return None
-
-    # Base case: return 1 if n is 1
-    if n == 1:
-        return 1
-
-    # Recursive case: calculate i^2 + summation_i_squared(n-1)
-    return n**2 + summation_i_squared(n-1)
+    numbers = np.arange(1, n+1)
+    return np.sum(numbers**2)
