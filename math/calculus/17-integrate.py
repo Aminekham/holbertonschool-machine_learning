5ccde84733b3bfea90d89882f6d74f150f238fea
Aminekham <lkham0508@gmail.com>
2023-12-16 20:24:13 +0100

    integrate

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+function to get the derivative of a polynominal function
+"""
+
+
+def poly_integral(poly, C=0):
+    """
+    Parameters:
+        derivative: list to save the coefficents of the derivative
+    """
+    if not isinstance(poly, list):
+        return None
+    if not all(isinstance(coeff, (int, float)) for coeff in poly):
+        return None
+    integral = []
+    integral.append(0)
+    for i in range(len(poly)):
+        print(integral)
+        integral.append(poly[i] / (i+1))
+    if sum(integral) == 0:
+        return [0]
+    return integral
