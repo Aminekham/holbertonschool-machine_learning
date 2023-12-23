9dc3a6e4b22a899e3647641394d5ca29aea48cb5
Aminekham <lkham0508@gmail.com>
2023-12-23 23:32:41 +0100

    readme

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -12,12 +12,14 @@ class Poisson:
         init function to check values and calculate a reasonable value for 
         the poisson rate
         """
-        self.lambtha = float(lambtha)
-        if data == []:
+        if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
-        self.lambtha = sum(data) / len(data)
-        if type(data) != list:
-            raise TypeError("data must be a list")
-        if len(data) < 2:
-            raise ValueError("data must contain multiple values")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(sum(data) / len(data))
+
