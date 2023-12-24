a3aae05ca7e6d0c7119ed6f2261a7d632ece2ea7
Aminekham <lkham0508@gmail.com>
2023-12-24 20:16:47 +0100

    documentation

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -28,7 +28,8 @@ class Exponential:
 
     def pdf(self, x):
         """
-        probability distribution function to search for the likelihood of a particular
+        probability distribution function to search for the 
+        likelihood of a particular
         random variable to be in a the needed range of values
         """
         if x < 0:
