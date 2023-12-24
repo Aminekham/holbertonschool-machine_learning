6687f85d22e40d0bd90fee53dc52d556dbd6c285
Aminekham <lkham0508@gmail.com>
2023-12-24 20:02:12 +0100

    working on pdf

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -32,4 +32,4 @@ class Exponential:
         """
         if x < 0:
             return 0
-        return(self.lambtha * 2.7182818285 ** -self.lambtha * x)
+        return(self.lambtha * 2.7182818285 **-(self.lambtha * x))
