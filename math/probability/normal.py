9bcd6206b2cac26557a5ec02e1296389a2661c92
Aminekham <lkham0508@gmail.com>
2023-12-24 23:32:11 +0100

    calculating the x and z (standard normal distribution

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 """
-This is our main exponential class
+This is our main normal class
 """
 
 
 class Normal:
     """
-    this defines the exponential class to
+    this defines the normal class to
     understand the core concepts of
-    exponential distribution
+    normal distribution
     """
     def __init__(self, data=None, mean=0., stddev=1.):
         """
         init function to check values and calculate a reasonable value for
-        the lambtha rate
+        the mean and standard diviation
         """
         if data is None:
             if stddev <= 0:
@@ -29,21 +29,9 @@ class Normal:
             self.mean = float(sum(data) / len(data))
             self.stddev = (sum((x - self.mean) ** 2 for x in data) / n) ** 0.5
 
-    def pdf(self, x):
-        """
-        probability distribution function to search for the
-        likelihood of a particular
-        random variable to be in a the needed range of values
-        """
-        if x < 0:
-            return 0
-        return(self.lambtha * 2.7182818285 ** - (self.lambtha * x))
-
-    def cdf(self, x):
-        """
-        cumulative distribution function to get the probability
-        of our random variable is less or equal to the value of x
-        """
-        if x < 0:
-            return 0
-        return(1 - 2.7182818285 ** - (self.lambtha * x))
+    def z_score(self, x):
+        z = x - self.mean / self.stddev
+        return(z)
+    def x_value(self, z):
+        x = z * self.stddev + self.mean
+        return(x)
