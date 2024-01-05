1e98dc377e23c6dae1f8789be3a4804b2d0c75a6
Aminekham <lkham0508@gmail.com>
2024-01-05 16:22:34 +0100

    creating the neuron

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import numpy as np
+"""
+Creating the neural network by our
+own
+"""
+
+
+class Neuron:
+    """
+    this is the neuron responsible for performing 
+    the classification task 
+    """
+    def __init__(self, nx):
+        """
+        initialisation function to get the 
+        weights entering the neuron
+        while defining the bias b and the output A
+        """
+        self.w = np.random.normal(nx)
+        self.b = 0
+        self.A = 0
+        if type(nx) != int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
