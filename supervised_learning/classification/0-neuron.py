d878e71bca1495ccc7b2d8d5afc5922014797909
Aminekham <lkham0508@gmail.com>
2024-01-05 23:31:31 +0100

    fixing weights

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -17,10 +17,10 @@ class Neuron:
         weights entering the neuron
         while defining the bias b and the output A
         """
-        self.W = np.random.normal(size = (1, nx))
-        self.b = 0
-        self.A = 0
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
+        self.W = np.random.normal(size = (1, nx))
+        self.b = 0
+        self.A = 0
