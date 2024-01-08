a0dfc1ae9fca979e0b4f86265269806b0daea377
Aminekham <lkham0508@gmail.com>
2024-01-08 23:37:42 +0100

    fixing the sum

--- supervised_learning/classification/3-neuron.py
+++ supervised_learning/classification/3-neuron.py
@@ -67,6 +67,6 @@ class Neuron:
         our neuron and return to
         reduce it in the next steps
         """
-        A = 1.0000001 - A
-        cost = -1/len(Y[0]) * np.sum(Y * np.log(A) + (1 - Y) * np.log(A))
+        sub = 1.0000001 - A
+        cost = -1/len(Y[0]) * np.sum(Y * np.log(A) + (1 - Y) * np.log(sub))
         return cost
