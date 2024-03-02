2624bd2e17804d6034a99f5b14bff690d696c8ab
Aminekham <lkham0508@gmail.com>
2024-03-02 01:07:08 +0100

    fixing doc and pycodestyle

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -7,13 +7,23 @@ allowing it to make predictions and load the data
 
 import tensorflow.keras as K
 
+
 class Yolo:
+    """
+    Yolo class to define the elements
+    and functions making the algorithm work
+    """
     model = None
     class_names = None
     class_t = None
     nms_t = None
     anchors = None
+
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """
+        initialize the attributes for the yolo
+        algorithm
+        """
         with open(classes_path, 'r') as file:
             classes_list = [line.strip() for line in file if line.strip()]
         self.model = K.models.load_model(model_path)
