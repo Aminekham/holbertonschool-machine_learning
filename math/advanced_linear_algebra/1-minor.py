a3e924940c1431642241ea382e98a932a5aa04f5
Aminekham <lkham0508@gmail.com>
2024-03-23 00:47:08 +0100

    the minor

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -6,10 +6,11 @@ determinant = __import__('0-determinant').determinant
 
 
 def minor(matrix):
-    if matrix == [] or not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+    state = all(isinstance(row, list) for row in matrix)
+    if matrix == [] or not isinstance(matrix, list) or not state:
         raise TypeError("matrix must be a list of lists")
     if len(matrix) != len(matrix[0]) or matrix[0] == []:
-        raise ValueError("matrix must be a square matrix")
+        raise ValueError("matrix must be a non-empty square matrix")
     num_rows = len(matrix)
     minors = []
     if len(matrix) == 1:
@@ -17,6 +18,7 @@ def minor(matrix):
     for i in range(num_rows):
         minor_row = []
         for j in range(num_rows):
-            minor_row.append(determinant([row[:j] + row[j+1:] for row in matrix[:i] + matrix[i+1:]]))
+            minor_row.append(determinant([row[:j] + row[j+1:]
+                                          for row in matrix[:i] + matrix[i+1:]]))
         minors.append(minor_row)
     return minors
