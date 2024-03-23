113161ece39d76ed1f0684595ccd1e35d4deba5d
Aminekham <lkham0508@gmail.com>
2024-03-23 05:36:55 +0100

    documentation + explanation

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
 """
-
+The definiteness of a matrix
 """
 import numpy as np
 
 
 def definiteness(matrix):
+    """
+    steps and mathematical explanation:
+    *testing if the matrix is symetric or not
+    *calculating its eigenvalues(How much a vector
+    is scaled after applying the matrix transformation)
+    *applying the conditions to define a matrix:
+    Positive definite: every vector points to the origin
+    when transformed by this matrix
+    Negative definite: the opposite of Positive definite
+    Semi definite: vectors in certain directions are zeros(
+    the ellipsoids touch the origin)
+    Indefinite: no consistent behavior
+    """
     if not isinstance(matrix, np.ndarray):
         raise TypeError("matrix must be a numpy.ndarray")
     transpose = np.transpose(matrix)
@@ -16,7 +29,7 @@ def definiteness(matrix):
         return "Positive definite"
     elif all(eig < 0 for eig in eigenvalues):
         return "Negative definite"
-    elif any(eig > 0 for eig in eigenvalues) and any(eigen < 0 
+    elif any(eig > 0 for eig in eigenvalues) and any(eigen < 0
                                                      for eigen in eigenvalues):
         return "Indefinite"
     elif all(eig >= 0 for eig in eigenvalues):
