97f7e1407e8118b8966bde3635a8f1fc3b6d338a
Aminekham <lkham0508@gmail.com>
2024-03-24 23:52:26 +0100

    pycodestyle

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -14,11 +14,11 @@ class MultiNormal:
         init function
         """
         if not isinstance(data, np.ndarray):
-            raise TypeError("data must be a 2D numpy.ndarray")        
+            raise TypeError("data must be a 2D numpy.ndarray")
         if data.ndim != 2:
-            raise TypeError("data must be a 2D numpy.ndarray")        
+            raise TypeError("data must be a 2D numpy.ndarray")  
         if data.shape[1] < 2:
-            raise ValueError("data must contain multiple data points")        
+            raise ValueError("data must contain multiple data points")
         self.mean = np.mean(data, axis=1, keepdims=True)
         centered_data = data - self.mean
         self.cov = np.dot(centered_data, centered_data.T) / (data.shape[1] - 1)
