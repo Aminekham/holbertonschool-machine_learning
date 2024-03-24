6eb1e2ebfa7b9861beed71e23dba31ff9ce3a395
Aminekham <lkham0508@gmail.com>
2024-03-24 02:59:38 +0100

    pycodestyle

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -4,12 +4,14 @@ Calculating the determinant
 of a certain matrix
 """
 
+
 def determinant(matrix):
     """
     The determinant is how much does
     this matrix change in the space
     """
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+    state = all(isinstance(row, list) for row in matrix)
+    if not isinstance(matrix, list) or not state:
         raise TypeError("matrix must be a list of lists")
     if len(matrix) != len(matrix[0]) and matrix[0] != []:
         raise ValueError("matrix must be a square matrix")
@@ -18,11 +20,13 @@ def determinant(matrix):
     if len(matrix) == 1 and len(matrix[0]) == 1:
         return matrix[0][0]
     if len(matrix) == 2:
-        det = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+        det1 = matrix[0][0] * matrix[1][1]
+        det2 = matrix[0][1] * matrix[1][0]
+        det = det1 - det2
         return det
     num_cols = len(matrix[0])
     det = 0
     for j in range(num_cols):
-        minor = [row[:j] + row[j+1:] for row in matrix[1:]]
-        det += (-1) ** j * matrix[0][j] * determinant(minor)
+        mnr = [row[:j] + row[j+1:] for row in matrix[1:]]
+        det += (-1) ** j * matrix[0][j] * determinant(mnr)
     return det
