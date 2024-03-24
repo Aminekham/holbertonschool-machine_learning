fd4766898b4e027b3f7077d225d9cccc4ff8fadb
Aminekham <lkham0508@gmail.com>
2024-03-24 03:16:18 +0100

    logic fix

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -13,8 +13,9 @@ def minor(matrix):
     state = all(isinstance(row, list) for row in matrix)
     if matrix == [] or not isinstance(matrix, list) or not state:
         raise TypeError("matrix must be a list of lists")
-    if (len(matrix) != all(len(smatrix) for smatrix in matrix) and matrix[0] != []) or matrix[0] == []:
-        raise ValueError("matrix must be a non-empty square matrix")
+    for smatrix in matrix:
+        if len(matrix) != len(smatrix) or smatrix == []:
+            raise ValueError("matrix must be a non-empty square matrix")
     num_rows = len(matrix)
     minors = []
     if len(matrix) == 1:
