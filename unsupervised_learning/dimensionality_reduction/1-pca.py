619d2a5faad50686e8f2a0049d7a69ed807f4403
Aminekham <lkham0508@gmail.com>
2024-03-31 17:44:51 +0200

    test test

--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -13,7 +13,10 @@ def pca(X, ndim):
     intensive than calculating the covariance matrix and getting the
     eigenvalues decomposition from it
     """
-    ls, singular_v, rs = np.linalg.svd(X)
-    W = rs.T
-    Wr = W[:ndim]
+    cov_matrix = np.cov(X, rowvar=False)
+    eigvals, eigvecs = np.linalg.eig(cov_matrix)
+    sorted_indices = np.argsort(eigvals)[::-1]
+    sorted_eigvecs = eigvecs[:, sorted_indices]
+    top_eigvecs = sorted_eigvecs[:, :ndim]
+    Wr = np.dot(X, top_eigvecs)
     return Wr
