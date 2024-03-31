6df673c5c4e7c411d8d36e18b07a1886c8d45eb0
Aminekham <lkham0508@gmail.com>
2024-03-31 18:15:57 +0200

    fixing conditions

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -18,13 +18,18 @@ def intersection(x, n, P, Pr):
         raise ValueError("x must be an integer that is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
-    if len(P.shape) != 1 or not isinstance(P, np.ndarray):
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
     for p in P:
         if p < 0 or p > 1:
             raise ValueError("All values in P must be in the range [0, 1]")
+    for pr in Pr:
+        if pr < 0 or pr > 1:
+            raise ValueError("All values in Pr must be in the range [0, 1]")
     if not np.isclose(np.sum(Pr), 1):
         raise ValueError("Pr must sum to 1")
+    if Pr.shape != P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
     res = 1
     for i in range(x):
         res = res * (n - i) // (i + 1)
