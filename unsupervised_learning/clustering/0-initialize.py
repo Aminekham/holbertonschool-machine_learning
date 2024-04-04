3a524a18c53c5d53f3333387f13d373502715131
Aminekham <lkham0508@gmail.com>
2024-04-04 14:19:39 +0200

    fixing the none condition

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -11,7 +11,10 @@ def initialize(X, k):
     Using the multivariate uniform
     distribution to have the intial values
     """
-    x_min = np.min(X, axis = 0)
-    x_max = np.max(X, axis = 0)
-    init = np.random.uniform(x_min, x_max, size = (k, X.shape[1]))
-    return init
+    try:
+        x_min = np.min(X, axis = 0)
+        x_max = np.max(X, axis = 0)
+        init = np.random.uniform(x_min, x_max, size = (k, X.shape[1]))
+        return init
+    except:
+        return None
