a2f137fd637b71cf98d92cfad0abef33c3e1dd71
Aminekham <lkham0508@gmail.com>
2024-04-07 21:42:42 +0200

    fixing the X condition

--- unsupervised_learning/clustering/4-initialize.py
+++ unsupervised_learning/clustering/4-initialize.py
@@ -9,6 +9,8 @@ kmeans = __import__('1-kmeans').kmeans
 
 def initialize(X, k):
     try:
+        if X.shape[1] == 1:
+            return None, None, None
         pi = np.full((k,), 1 / k)
         centroids, _ = kmeans(X, k)
         d = X.shape[1]
