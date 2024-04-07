879cbf52c8183c6e3a57d97724172c94f21a4028
Aminekham <lkham0508@gmail.com>
2024-04-07 22:08:26 +0200

    expectation step

---
+++ unsupervised_learning/clustering/6-expectation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+
+"""
+import numpy as np
+pdf = __import__('5-pdf').pdf
+
+
+def expectation(X, pi, m, S):
+    """
+    
+    """
+    k = pi.shape[0]
+    n, d = X.shape
+    g = np.zeros((k, n))
+    for i in range(k):
+        g[i] = pi[i] * pdf(X, m[i], S[i])
+    l = np.sum(np.log(np.sum(g, axis=0)))
+    g /= np.sum(g, axis=0)
+    return g, l
