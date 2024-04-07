50a6dc451eaaf77d26fea4702d9ed203b0cfe7f9
Aminekham <lkham0508@gmail.com>
2024-04-07 22:22:27 +0200

    updating the parameters to better fit the data

---
+++ unsupervised_learning/clustering/7-maximization.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+
+"""
+import numpy as np
+
+
+def maximization(X, g):
+    """
+    
+    """
+    k, n = g.shape
+    d = X.shape[1]
+    pi = np.sum(g, axis=1) / n
+    m = np.dot(g, X) / np.expand_dims(np.sum(g, axis=1), axis=1)
+    S = np.zeros((k, d, d))
+    for i in range(k):
+        diff = X - m[i]
+        S[i] = np.dot(g[i] * diff.T, diff) / np.sum(g[i])
+    return pi, m, S
