db394124a2da9aa5f899bab917182ff54de26692
Aminekham <lkham0508@gmail.com>
2024-04-07 13:01:36 +0200

    initialize gmm parameters

--- unsupervised_learning/clustering/4-initialize.py
+++ unsupervised_learning/clustering/4-initialize.py
@@ -11,8 +11,8 @@ def initialize(X, k):
     pi = np.full((k,), 1 / k)
     try:
         centroids, _ = kmeans(X, k)
+        d = X.shape[1]
+        covariances = np.tile(np.eye(d), (k, 1, 1))
+        return pi, centroids, covariances
     except Exception:
         return None, None, None
-    d = X.shape[1]
-    covariances = np.tile(np.eye(d), (k, 1, 1))
-    return pi, centroids, covariances
