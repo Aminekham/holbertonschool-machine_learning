09094d5b29c49ff9349cec4c065990c0845be2f8
Aminekham <lkham0508@gmail.com>
2024-04-07 22:31:08 +0200

    the input conditions

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -9,12 +9,15 @@ def maximization(X, g):
     """
     
     """
-    k, n = g.shape
-    d = X.shape[1]
-    pi = np.sum(g, axis=1) / n
-    m = np.dot(g, X) / np.expand_dims(np.sum(g, axis=1), axis=1)
-    S = np.zeros((k, d, d))
-    for i in range(k):
-        diff = X - m[i]
-        S[i] = np.dot(g[i] * diff.T, diff) / np.sum(g[i])
-    return pi, m, S
+    try:
+        k, n = g.shape
+        d = X.shape[1]
+        pi = np.sum(g, axis=1) / n
+        m = np.dot(g, X) / np.expand_dims(np.sum(g, axis=1), axis=1)
+        S = np.zeros((k, d, d))
+        for i in range(k):
+            diff = X - m[i]
+            S[i] = np.dot(g[i] * diff.T, diff) / np.sum(g[i])
+        return pi, m, S
+    except Exception as e:
+        return None, None, None
