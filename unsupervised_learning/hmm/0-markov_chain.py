cba9afdda449f48392c3b5053cef15bf2f750475
Aminekham <lkham0508@gmail.com>
2024-04-09 12:22:55 +0200

    documentation and pycodestyle

--- unsupervised_learning/hmm/0-markov_chain.py
+++ unsupervised_learning/hmm/0-markov_chain.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 """
-
+The markov chain
 """
 import numpy as np
 
 
 def markov_chain(P, s, t=1):
     """
-    
+    Calculating the probability to be at a certain state
+    after t iterations
+    We used the matrix exponentiation because of the nature of
+    markov chains and that the current state depends only from
+    the previous one which means that raising to the expopent gives
+    the needed matrix after the needed t times
     """
     try:
         after_t = np.dot(s, np.linalg.matrix_power(P, t))
