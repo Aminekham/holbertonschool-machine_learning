c3f967bfcb858004925dccc6c513ac3dd26c961f
Aminekham <lkham0508@gmail.com>
2024-04-10 03:29:38 +0200

    logic testing

---
+++ unsupervised_learning/hmm/1-regular.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+
+"""
+import numpy as np
+
+
+def regular(P):
+    """
+    
+    """
+    try:
+        values, right_eigen = np.linalg.eig(P.T)
+        idx = np.where(np.isclose(values, 1.0))[0]
+        if len(idx) != 1:
+            return None
+        left_eigen = np.real(right_eigen[:, idx])
+        left_eigen /= np.sum(left_eigen)
+        return left_eigen.flatten()
+    except Exception as e:
+        return None
