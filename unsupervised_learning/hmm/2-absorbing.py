38acbdf1165ae80fb52686d78095fdfdf3add3c7
Aminekham <lkham0508@gmail.com>
2024-04-15 00:41:05 +0200

    final absorbing file

--- unsupervised_learning/hmm/2-absorbing.py
+++ unsupervised_learning/hmm/2-absorbing.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
 """
-
+Cheking if a markov chain
+have an absorbing state or not
 """
 import numpy as np
 
 
 def absorbing(P):
     """
-
+    an absorbing state is a state
+    that have a 1 probability to itself
+    which means like its a black hole
     """
     length = len(P)
     for i in range(length):
