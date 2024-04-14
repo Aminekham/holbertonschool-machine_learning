35c66c4ff98449b511591c6b0806d883d5b074d4
Aminekham <lkham0508@gmail.com>
2024-04-15 00:08:46 +0200

    pycode style

--- unsupervised_learning/hmm/3-forward.py
+++ unsupervised_learning/hmm/3-forward.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """
-
+comment
 """
 import numpy as np
 
 
 def forward(Observation, Emission, Transition, Initial):
     """
-
+    comment
     """
     T = len(Observation)
     N, M = Emission.shape
@@ -15,9 +15,6 @@ def forward(Observation, Emission, Transition, Initial):
     F[:, 0] = Initial[:, 0] * Emission[:, Observation[0]]
     for t in range(1, T):
         for j in range(N):
-            F[j, t] = np.sum(F[i,
-                               t-1] * Transition[i,
-                                                    j] * Emission[j,
-                                                                  Observation[t]] for i in range(N))
+            F[j, t] = np.sum(F[i, t-1] * Transition[i, j] * Emission[j, Observation[t]] for i in range(N))
     P = np.sum(F[:, -1])
     return P, F
