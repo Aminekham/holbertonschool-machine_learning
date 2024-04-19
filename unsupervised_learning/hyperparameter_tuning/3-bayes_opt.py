c96b0c1e4481fbc5f044363f961be50b8d345047
Aminekham <lkham0508@gmail.com>
2024-04-19 20:19:21 +0200

    init doc and pycodestyle

--- unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 """
-
+The bayessian optimization
+class file
 """
 import numpy as np
 GP = __import__('2-gp').GaussianProcess
 
 
 class BayesianOptimization:
+    """
+    the bayessian optimization class which is
+    using the gaussian  process method to pick the best possible parameters
+    based on an accusation function
+    """
     def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1, xsi=0.01, minimize=True):
+        """
+        Initializing the bayessian optimization parameters + GP object
+        """
         self.f = f
         self.gp = GP(X_init, Y_init, l, sigma_f)
-        self.X_s = np.where(bounds[0] < X_init)
-        self.X_s = np.array(np.where(X_init < bounds[1]))
+        self.X_s = np.linspace(bounds[0], bounds[1], ac_samples).reshape(-1, 1)
         self.xsi = xsi
         self.minimize = minimize
