5e860a86ac7d742b4cd5906cc2c3f25870751a53
Aminekham <lkham0508@gmail.com>
2024-04-21 22:05:26 +0200

    pycodestyle

--- unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/4-bayes_opt.py
@@ -24,6 +24,7 @@ class BayesianOptimization:
         self.X_s = np.linspace(bounds[0], bounds[1], ac_samples).reshape(-1, 1)
         self.xsi = xsi
         self.minimize = minimize
+
     def acquisition(self):
         """
         defining the acquisition function u
