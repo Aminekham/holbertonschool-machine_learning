f7f4d508ab3491cd29bfd4d887cb7b41fda4330d
Aminekham <lkham0508@gmail.com>
2024-04-26 11:55:01 +0200

    fixing import

--- unsupervised_learning/autoencoders/0-vanilla.py
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -3,24 +3,22 @@
 building a vanilla
 autoencoder: encoder and decoder
 """
-import numpy as np
-import tensorflow as tf
-
+import tensorflow.keras as keras
 
 def autoencoder(input_dims, hidden_layers, latent_dims):
     """
     
     """
-    input = tf.keras.Input(shape=(input_dims, ))
+    input = keras.Input(shape=(input_dims, ))
     layer = input
     for node in hidden_layers:
-        layer = tf.keras.Dense(node, activation='relu')(layer)
-    latent = tf.keras.Dense(latent_dims, activation='relu')(layer)
-    encoder = tf.keras.models.Model(input, latent)
+        layer = keras.Dense(node, activation='relu')(layer)
+    latent = keras.Dense(latent_dims, activation='relu')(layer)
+    encoder = keras.models.Model(input, latent)
     layer = latent
     for node in reversed(hidden_layers):
-        layer = tf.keras.Dense(node, activation='relu')(layer)
-    decoder_layer = tf.keras.Dense(input_dims, activation='sigmoid')(layer)
-    decoder = tf.keras.Model(latent, decoder_layer)
-    autoencoder = tf.keras.Model(input, decoder_layer)
+        layer = keras.Dense(node, activation='relu')(layer)
+    decoder_layer = keras.Dense(input_dims, activation='sigmoid')(layer)
+    decoder = keras.Model(latent, decoder_layer)
+    autoencoder = keras.Model(input, decoder_layer)
     return encoder, decoder, autoencoder
