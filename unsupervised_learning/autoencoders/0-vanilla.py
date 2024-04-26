3d37e6de72cd3234b1cb0d233f5b1ab2c414aca0
Aminekham <lkham0508@gmail.com>
2024-04-26 12:14:41 +0200

    fixing

--- unsupervised_learning/autoencoders/0-vanilla.py
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -12,13 +12,13 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     input = keras.Input(shape=(input_dims, ))
     layer = input
     for node in hidden_layers:
-        layer = keras.Dense(node, activation='relu')(layer)
-    latent = keras.Dense(latent_dims, activation='relu')(layer)
+        layer = keras.layers.Dense(node, activation='relu')(layer)
+    latent = keras.layers.Dense(latent_dims, activation='relu')(layer)
     encoder = keras.models.Model(input, latent)
     layer = latent
     for node in reversed(hidden_layers):
-        layer = keras.Dense(node, activation='relu')(layer)
-    decoder_layer = keras.Dense(input_dims, activation='sigmoid')(layer)
+        layer = keras.layers.Dense(node, activation='relu')(layer)
+    decoder_layer = keras.layers.Dense(input_dims, activation='sigmoid')(layer)
     decoder = keras.Model(latent, decoder_layer)
     autoencoder = keras.Model(input, decoder_layer)
     return encoder, decoder, autoencoder
