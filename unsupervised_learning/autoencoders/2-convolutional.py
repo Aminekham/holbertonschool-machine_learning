7d0ebc19740fd5028545a927bf4c39942250348e
Aminekham <lkham0508@gmail.com>
2024-04-27 17:19:26 +0200

    fixing layers

--- unsupervised_learning/autoencoders/2-convolutional.py
+++ unsupervised_learning/autoencoders/2-convolutional.py
@@ -8,15 +8,18 @@ def autoencoder(input_dims, filters, latent_dims):
     encoder_inputs = keras.layers.Input(shape=input_dims)
     x = encoder_inputs
     for f in filters:
-        x = keras.layers.Conv2D(f, (3, 3), activation='relu', padding='same')(x)
+        x = keras.layers.Conv2D(f, (3, 3), activation='relu',
+                                padding='same')(x)
         x = keras.layers.MaxPooling2D((2, 2), padding='same')(x)
     encoder = keras.models.Model(encoder_inputs, x)
     decoder_inputs = keras.layers.Input(shape=latent_dims)
     x = decoder_inputs
     for f in reversed(filters):
-        x = keras.layers.Conv2D(f, (3, 3), activation='relu', padding='same')(x)
+        x = keras.layers.Conv2D(f, (3, 3), activation='relu',
+                                padding='same')(x)
         x = keras.layers.UpSampling2D((2, 2))(x)
-    x = keras.layers.Conv2D(input_dims[-1], (3, 3), activation='sigmoid', padding='same')(x)
+    x = keras.layers.Conv2D(input_dims[-1], (3, 3),
+                            activation='sigmoid', padding='same')(x)
     decoder_outputs = x
     decoder = keras.models.Model(decoder_inputs, decoder_outputs)
     autoencoder_outputs = decoder(encoder(encoder_inputs))
