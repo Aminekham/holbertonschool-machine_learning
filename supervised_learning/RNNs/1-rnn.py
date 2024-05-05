8ed4ab75a4b5729df37fb09b1c0985817eacd59f
Aminekham <lkham0508@gmail.com>
2024-05-05 18:28:41 +0200

    fixing the shapes

--- supervised_learning/RNNs/1-rnn.py
+++ supervised_learning/RNNs/1-rnn.py
@@ -7,14 +7,13 @@ import numpy as np
 
 def rnn(rnn_cell, X, h_0):
     """
-    
+
     """
-    t, m, i = X.shape
+    t, m, _ = X.shape
     h = h_0.shape[1]
     H = np.zeros((t + 1, m, h))
     Y = np.zeros((t, m, rnn_cell.by.shape[1]))
     H[0] = h_0
     for step in range(t):
-        H[step + 1] = rnn_cell.forward(H[step], X[step])
-        Y[step] = np.dot(H[step + 1], rnn_cell.Wy) + rnn_cell.by
-    return H[1:], Y
+        H[step + 1], Y[step] = rnn_cell.forward(H[step], X[step])
+    return H, Y
