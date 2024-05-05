01edc0b2598efa4bb14530c9bbc2f67f2b481e4e
Aminekham <lkham0508@gmail.com>
2024-05-05 18:20:16 +0200

    fixing the logic

--- supervised_learning/RNNs/1-rnn.py
+++ supervised_learning/RNNs/1-rnn.py
@@ -7,15 +7,14 @@ import numpy as np
 
 def rnn(rnn_cell, X, h_0):
     """
-
+    
     """
     t, m, i = X.shape
     h = h_0.shape[1]
-    H = np.zeros((t, m, h))
-    Y = np.zeros((t, m, rnn_cell.Wy.shape[1]))
-    h_next = h_0
+    H = np.zeros((t + 1, m, h))
+    Y = np.zeros((t, m, rnn_cell.by.shape[1]))
+    H[0] = h_0
     for step in range(t):
-        h_next, y = rnn_cell.forward(h_next, X[step])
-        H[step] = h_next
-        Y[step] = y
-    return H, Y
+        H[step + 1] = rnn_cell.forward(H[step], X[step])
+        Y[step] = np.dot(H[step + 1], rnn_cell.Wy) + rnn_cell.by
+    return H[1:], Y
