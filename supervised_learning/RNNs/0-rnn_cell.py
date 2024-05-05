2de3a3d5a53529c127d2b6e877ed5b343cfd44a4
Aminekham <lkham0508@gmail.com>
2024-05-05 02:43:54 +0200

    rnn forward

---
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+
+"""
+import numpy as np
+
+
+class RNNCell:
+    """
+
+    """
+    def __init__(self, i, h, o):
+        """
+        
+        """
+        self.Wh = np.random.randn(h + i, h)
+        self.bh = np.zeros((1, h))
+        self.Wy = np.random.randn(h, o)
+        self.by = np.zeros((1, o))
+    def forward(self, h_prev, x_t):
+        concat_input = np.concatenate((h_prev, x_t), axis=1)
+        h_next = np.tanh(np.dot(concat_input, self.Wh) + self.bh)
+        y = np.dot(h_next, self.Wy) + self.by
+        y = np.exp(y - np.max(y, axis=1, keepdims=True))
+        y = y / np.sum(y, axis=1, keepdims=True)
+        return h_next, y
