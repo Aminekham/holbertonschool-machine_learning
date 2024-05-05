450c2cc612e1687e71ba0146b27e333332f1c6c7
Aminekham <lkham0508@gmail.com>
2024-05-05 19:10:28 +0200

    deep multilayer rnn

---
+++ supervised_learning/RNNs/4-deep_rnn.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+
+"""
+import numpy as np
+RNNCell = __import__('0-rnn_cell').RNNCell
+
+
+def deep_rnn(rnn_cells, X, h_0):
+    """
+
+    """
+    T, m, i = X.shape
+    l, _, h = h_0.shape
+    H = np.zeros((T + 1, l, m, h))
+    Y = np.zeros((T, m, rnn_cells[-1].Wy.shape[1]))
+    H[0] = h_0
+    for t in range(T):
+        for layer, cell in enumerate(rnn_cells):
+            if layer == 0:
+                H[t + 1, layer], Y[t] = cell.forward(H[t, layer], X[t])
+            else:
+                H[t + 1, layer], _ = cell.forward(H[t, layer], H[t + 1, layer - 1])
+    return H[1:], Y
