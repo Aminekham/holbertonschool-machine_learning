e811c0935f23f0a6e3cb5fa42b747a274d3d70cb
Aminekham <lkham0508@gmail.com>
2024-05-05 19:02:22 +0200

    long short term memory

---
+++ supervised_learning/RNNs/3-lstm_cell.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+
+"""
+import numpy as np
+
+
+class LSTMCell:
+    """
+
+    """
+    def __init__(self, i, h, o):
+        """
+
+        """
+        self.Wf = np.random.randn(h + i, h)
+        self.bf = np.zeros((1, h))
+        self.Wu = np.random.randn(h + i, h)
+        self.bu = np.zeros((1, h))
+        self.Wc = np.random.randn(h + i, h)
+        self.bc = np.zeros((1, h))
+        self.Wo = np.random.randn(h + i, h)
+        self.bo = np.zeros((1, h))
+        self.Wy = np.random.randn(h, o)
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, c_prev, x_t):
+        """
+
+        """
+        concat = np.concatenate((h_prev, x_t), axis=1)
+        fg = np.dot(concat, self.Wf) + self.bf
+        fg = 1 / (1 + np.exp(-fg))
+        ug = np.dot(concat, self.Wu) + self.bu
+        ug = 1 / (1 + np.exp(-ug))
+        c_temp = np.tanh(np.dot(concat, self.Wc) + self.bc)
+        c_next = fg * c_prev + ug * c_temp
+        og = np.dot(concat, self.Wo) + self.bo
+        og = 1 / (1 + np.exp(-og))        
+        h_next = og * np.tanh(c_next)
+        y = np.dot(h_next, self.Wy) + self.by
+        y = np.exp(y) / np.sum(np.exp(y), axis=1, keepdims=True)
+        return h_next, c_next, y
