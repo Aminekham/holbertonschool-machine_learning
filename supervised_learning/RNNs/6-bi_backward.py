24b2dcf6e6f2ebf6e1b3a631eb44a6b8c5892c8f
Aminekham <lkham0508@gmail.com>
2024-05-06 00:05:36 +0200

    backward same as forward but reversed

--- supervised_learning/RNNs/6-bi_backward.py
+++ supervised_learning/RNNs/6-bi_backward.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python3
 """
-
+The bidirectional rnn
 """
 import numpy as np
 
 
 class BidirectionalCell:
     """
-
+    A single cell of the bidirectional rnn
+    which can basically learn the data on a two scale
+    on forward and backward so it gets the maximum of features
     """
     def __init__(self, i, h, o):
         """
-
+        The init for all the used weights and biases for the forward
+        and backward learning
         """
         self.Whf = np.random.randn(i + h, h)
         self.Whb = np.random.randn(i + h, h)
@@ -22,13 +25,18 @@ class BidirectionalCell:
 
     def forward(self, h_prev, x_t):
         """
-
+        the forward which is basically a simple RNN
+        hidden forward state
         """
         h_x_concat = np.concatenate((h_prev, x_t), axis=1)
         h_next = np.tanh(np.dot(h_x_concat, self.Whf) + self.bhf)
         return h_next
 
     def backward(self, h_next, x_t):
+        """
+        the backward which is basically reversing the forward through
+        our hidden states
+        """
         h_x = np.concatenate((h_next, x_t), axis=1)
         h_prev = np.tanh(np.dot(h_x, self.Whb) + self.bhb)
         return h_prev
