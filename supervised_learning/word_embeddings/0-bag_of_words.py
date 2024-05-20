5ad15cf510b70f29aa7d3e5313e9be980bdae8bc
Aminekham <lkham0508@gmail.com>
2024-05-20 14:55:21 +0200

    bag of words with word vectorizer

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
 import numpy as np
-from sklearn.feature_extraction.text import CountVectorizer
+import re
+
+def preprocess_sentence(sentence):
+    processed_sentence = re.sub(r"\b(\w+)'s\b", r"\1", sentence.lower())
+    return re.findall(r'\w+', processed_sentence)
 
 def bag_of_words(sentences, vocab=None):
-    if vocab is not None:
-        vectorizer = CountVectorizer(vocabulary=vocab)
-    else:
-        vectorizer = CountVectorizer()
-    
-    X = vectorizer.fit_transform(sentences)
-    embeddings = X.toarray()
-    features = vectorizer.get_feature_names_out()
-    
-    return embeddings, features
+    preprocessed_sentences = [preprocess_sentence(sentence) for sentence in sentences]
+    if vocab is None:
+        all_words = [word for sentence in preprocessed_sentences for word in sentence]
+        vocab = sorted(set(all_words))
+    word_to_index = {word: idx for idx, word in enumerate(vocab)}
+    embeddings = np.zeros((len(sentences), len(vocab)), dtype=int)
+    for i, sentence in enumerate(preprocessed_sentences):
+        for word in sentence:
+            if word in word_to_index:
+                embeddings[i, word_to_index[word]] += 1
+    return embeddings, vocab
