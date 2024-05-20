efb58e600dba1f08f14156ccc919be5e61aef80e
Aminekham <lkham0508@gmail.com>
2024-05-20 14:56:45 +0200

    bag of words with word vectorizer

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -2,11 +2,21 @@
 import numpy as np
 import re
 
+
+"""
+
+"""
 def preprocess_sentence(sentence):
+    """
+    
+    """
     processed_sentence = re.sub(r"\b(\w+)'s\b", r"\1", sentence.lower())
     return re.findall(r'\w+', processed_sentence)
 
 def bag_of_words(sentences, vocab=None):
+    """
+    
+    """
     preprocessed_sentences = [preprocess_sentence(sentence) for sentence in sentences]
     if vocab is None:
         all_words = [word for sentence in preprocessed_sentences for word in sentence]
