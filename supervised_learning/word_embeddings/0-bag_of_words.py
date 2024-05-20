3d75f64bbfe51eab9d686222cb0f4938586b997d
Aminekham <lkham0508@gmail.com>
2024-05-20 14:45:09 +0200

    bag of words with word vectorizer

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -1,24 +1,15 @@
 #!/usr/bin/env python3
 import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
 
-"""
-
-"""
 def bag_of_words(sentences, vocab=None):
-    """
+    if vocab is not None:
+        vectorizer = CountVectorizer(vocabulary=vocab)
+    else:
+        vectorizer = CountVectorizer()
     
-    """
-    tokenized_sentences = [sentence.split() for sentence in sentences]
-    if vocab is None:
-        all_words = set(word for sentence in tokenized_sentences for word in sentence)
-        vocab = sorted(list(all_words))
-    embeddings = []
-    for sentence in tokenized_sentences:
-        embd = []
-        for word in sentence:
-            if word in vocab:
-                embd.append(1)
-            else:
-                embd.append(0)
-        embeddings.append(embd)    
-    return embeddings, vocab
+    X = vectorizer.fit_transform(sentences)
+    embeddings = X.toarray()
+    features = vectorizer.get_feature_names_out()
+    
+    return embeddings, features
