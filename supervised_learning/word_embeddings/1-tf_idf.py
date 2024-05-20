bb6705b6825abba71e4b83bd55e2235126018658
Aminekham <lkham0508@gmail.com>
2024-05-20 17:24:42 +0200

    term frequency inverse document frequency

--- supervised_learning/word_embeddings/1-tf_idf.py
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -2,14 +2,20 @@
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+"""
+
+"""
+
+
 def tf_idf(sentences, vocab=None):
+    """
+    
+    """
     if vocab is None:
         vectorizer = TfidfVectorizer()
     else:
         vectorizer = TfidfVectorizer(vocabulary=vocab)
-    
     X = vectorizer.fit_transform(sentences)
     embeddings = X.toarray()
     features = vectorizer.get_feature_names()
-    
     return embeddings, features
