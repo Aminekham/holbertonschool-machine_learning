462b5459cf247ec3020d421e2c97fd507d6df739
Aminekham <lkham0508@gmail.com>
2024-05-20 19:13:41 +0200

    word to vector

---
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from gensim.models import Word2Vec
+"""
+
+"""
+
+
+def word2vec_model(sentences, size=100, min_count=5, window=5, negative=5, cbow=True, iterations=5, seed=0, workers=1):
+    """
+
+    """
+    sg = 0
+    if cbow:
+        sg = 1
+    model = Word2Vec(sentences, size=size, min_count=min_count, window=window, negative=negative, sg=sg, iter=iterations, seed=seed, workers=workers)
+    return model
