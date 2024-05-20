6bf96bab18c3b9aeb0046774efe0371000af721b
Aminekham <lkham0508@gmail.com>
2024-05-20 14:11:21 +0200

    bow after fixing github

--- supervised_learning/word_embeddings/0-bag_of_words.py
+++ supervised_learning/word_embeddings/0-bag_of_words.py
@@ -12,16 +12,13 @@ def bag_of_words(sentences, vocab=None):
     if vocab is None:
         all_words = set(word for sentence in tokenized_sentences for word in sentence)
         vocab = sorted(list(all_words))
-    word_to_index = {word: idx for idx, word in enumerate(vocab)}
-    embeddings = np.zeros((len(sentences), len(vocab)), dtype=int)
-    for i, sentence in enumerate(tokenized_sentences):
-        word_counts = {}
+    embeddings = []
+    for sentence in tokenized_sentences:
+        embd = []
         for word in sentence:
-            if word in word_counts:
-                word_counts[word] += 1
+            if word in vocab:
+                embd.append(1)
             else:
-                word_counts[word] = 1
-        for word, count in word_counts.items():
-            if word in word_to_index:
-                embeddings[i, word_to_index[word]] = count
+                embd.append(0)
+        embeddings.append(embd)    
     return embeddings, vocab
