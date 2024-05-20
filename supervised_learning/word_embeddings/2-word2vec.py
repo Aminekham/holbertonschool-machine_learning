36ba9901a5cf5a15d78552a2b28d4fd22a4d2e46
Aminekham <lkham0508@gmail.com>
2024-05-20 20:16:53 +0200

    fixing

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -12,5 +12,11 @@ def word2vec_model(sentences, size=100, min_count=5, window=5, negative=5, cbow=
     sg = 0
     if cbow:
         sg = 1
-    model = Word2Vec(sentences, size=size, min_count=min_count, window=window, negative=negative, sg=sg, iter=iterations, seed=seed, workers=workers)
+    model = Word2Vec(sentences, size=size,
+                     min_count=min_count, window=window,
+                     negative=negative, sg=sg, iter=iterations,
+                     seed=seed, workers=workers)
+    model.train(sentences=sentences,
+                total_examples=model.corpus_count,
+                epochs=iterations)
     return model
