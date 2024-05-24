83dc4f98a0d6895241e9b3ac3f9cdcb0224eb60e
Aminekham <lkham0508@gmail.com>
2024-05-24 18:06:00 +0200

    sinusoidal positional encoding

---
+++ supervised_learning/attention/4-positional_encoding.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import numpy as np
+"""
+
+"""
+
+
+def positional_encoding(max_seq_len, dm):
+    """
+    
+    """
+    positional_encoding_matrix = np.zeros((max_seq_len, dm))
+    for pos in range(max_seq_len):
+        for i in range(dm):
+            angle = pos / np.power(10000, 2 * i / dm)
+            if i % 2 == 0:
+                positional_encoding_matrix[pos, i] = np.sin(angle)
+            else:
+                positional_encoding_matrix[pos, i] = np.cos(angle)
+    return positional_encoding_matrix
