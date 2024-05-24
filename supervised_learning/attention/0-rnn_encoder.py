4cd4fdee79cacfd8bbeb2f29b2c25e676c618494
Aminekham <lkham0508@gmail.com>
2024-05-24 14:38:28 +0200

    fixing imports

--- supervised_learning/attention/0-rnn_encoder.py
+++ supervised_learning/attention/0-rnn_encoder.py
@@ -7,13 +7,12 @@ import tensorflow as tf
 
 class RNNEncoder(tf.keras.layers.Layer):
     """
-    
+
     """
     def __init__(self, vocab, embedding, units, batch):
         """
-        
+
         """
-        super(RNNEncoder, self).__init__()
         self.batch = batch
         self.units = units
         self.embedding = tf.keras.layers.Embedding(vocab, embedding)
