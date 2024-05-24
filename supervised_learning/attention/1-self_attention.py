dde828d00680e2847a320d19ad406a3f519cb339
Aminekham <lkham0508@gmail.com>
2024-05-24 14:49:37 +0200

    pycode style

--- supervised_learning/attention/1-self_attention.py
+++ supervised_learning/attention/1-self_attention.py
@@ -13,7 +13,8 @@ class SelfAttention(tf.keras.layers.Layer):
         self.V = tf.keras.layers.Dense(1)
 
     def call(self, s_prev, hidden_states):
-        score = self.V(tf.nn.tanh(self.W(tf.expand_dims(s_prev, 1)) + self.U(hidden_states)))
+        score = self.V(tf.nn.tanh(self.W(tf.expand_dims(s_prev, 1))
+                                  + self.U(hidden_states)))
         weights = tf.nn.softmax(score, axis=1)
         context = tf.reduce_sum(weights * hidden_states, axis=1)
         return context, weights
