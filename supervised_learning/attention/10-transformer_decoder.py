72e731a5e903a829bf07d54a9727ff6cb4da4f40
Aminekham <lkham0508@gmail.com>
2024-05-26 19:34:46 +0200

    pycodestyle

--- supervised_learning/attention/10-transformer_decoder.py
+++ supervised_learning/attention/10-transformer_decoder.py
@@ -8,13 +8,15 @@ DecoderBlock = __import__('8-transformer_decoder_block').DecoderBlock
 
 
 class Decoder(tf.keras.layers.Layer):
-    def __init__(self, N, dm, h, hidden, target_vocab, max_seq_len, drop_rate=0.1):
+    def __init__(self, N, dm, h, hidden,
+                 target_vocab, max_seq_len, drop_rate=0.1):
         super(Decoder, self).__init__()
         self.N = N
         self.dm = dm
         self.embedding = tf.keras.layers.Embedding(target_vocab, dm)
         self.positional_encoding = positional_encoding(max_seq_len, dm)
-        self.blocks = [DecoderBlock(dm, h, hidden, drop_rate) for _ in range(N)]
+        self.blocks = [DecoderBlock(dm, h, hidden,
+                                    drop_rate) for _ in range(N)]
         self.dropout = tf.keras.layers.Dropout(drop_rate)
 
     def call(self, x, encoder_output, training, look_ahead_mask, padding_mask):
@@ -24,5 +26,6 @@ class Decoder(tf.keras.layers.Layer):
         x += self.positional_encoding[:seq_len, :]
         x = self.dropout(x, training=training)
         for i, block in enumerate(self.blocks):
-            x = block(x, encoder_output, training, look_ahead_mask, padding_mask)
+            x = block(x, encoder_output, training,
+                      look_ahead_mask, padding_mask)
         return x
