ef16e5e3a6aac0166e83be483a85f65c32a6503f
Aminekham <lkham0508@gmail.com>
2024-05-26 18:00:42 +0200

    pycodestyle

--- supervised_learning/attention/6-multihead_attention.py
+++ supervised_learning/attention/6-multihead_attention.py
@@ -5,6 +5,7 @@
 import tensorflow as tf
 sdp_attention = __import__('5-sdp_attention').sdp_attention
 
+
 class MultiHeadAttention(tf.keras.layers.Layer):
     def __init__(self, dm, h):
         """
@@ -39,6 +40,7 @@ class MultiHeadAttention(tf.keras.layers.Layer):
         V = self.split_heads(V, batch_size)
         scaled_attention, attention_weights = sdp_attention(Q, K, V, mask)
         scaled_attention = tf.transpose(scaled_attention, perm=[0, 2, 1, 3])
-        concat_attention = tf.reshape(scaled_attention, (batch_size, -1, self.dm))
+        concat_attention = tf.reshape(scaled_attention,
+                                      (batch_size, -1, self.dm))
         output = self.linear(concat_attention)
         return output, attention_weights
