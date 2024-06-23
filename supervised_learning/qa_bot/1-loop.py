97f5df1d18b24d959b682f9bd335ee601d17046c
Aminekham <lkham0508@gmail.com>
2024-06-23 23:06:24 +0200

    the main QA loop

---
+++ supervised_learning/qa_bot/1-loop.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+
+"""
+while(1):
+    x = input("Q:")
+    x = x.lower()
+    if x == "exit" or x == "quit" or x == "goodbye" or x == "bye":
+        print("A: Goodbye")
+        break
+    print("A:")
