cbc38a7243b5d9056f2eaa9df76b2dc659b4ed0c
Aminekham <lkham0508@gmail.com>
2024-06-26 09:35:52 +0200

    q learning init

--- reinforcement_learning/q_learning/1-q_init.py
+++ reinforcement_learning/q_learning/1-q_init.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 import numpy as np
 """
-
+Init for the Q-table
 """
 def q_init(env):
     """
-    
+    Q-table as a numpy.ndarray of zeros with shape
+    (number_of_states, number_of_actions)
     """
     return np.zeros((env.observation_space.n, env.action_space.n))
