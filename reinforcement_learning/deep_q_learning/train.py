48e3d17f71fb24d0f60efd1e9019e8041ec44cc2
Aminekham <lkham0508@gmail.com>
2024-07-11 18:23:36 +0200

    main train adding again

---
+++ reinforcement_learning/deep_q_learning/train.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import gym
+from keras.models import Sequential
+from keras.layers import Dense, Flatten
+from keras.optimizers import Adam
+from rl.agents.dqn import DQNAgent
+from rl.policy import EpsGreedyQPolicy
+from rl.memory import SequentialMemory
+
+env = gym.make('Breakout-v0')
+nb_actions = env.action_space.n
+model = Sequential()
+model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
+model.add(Dense(24, activation='relu'))
+model.add(Dense(24, activation='relu'))
+model.add(Dense(nb_actions, activation='linear'))
+memory = SequentialMemory(limit=50000, window_length=4)
+policy = EpsGreedyQPolicy()
+dqn = DQNAgent(model=model, nb_actions=nb_actions, memory=memory, nb_steps_warmup=10, target_model_update=1e-2, policy=policy)
+dqn.compile(Adam(learning_rate=1e-3), metrics=['mae'])
+dqn.fit(env, nb_steps=50000, visualize=True, verbose=2)
+dqn.save_weights('policy.h5', overwrite=True)
