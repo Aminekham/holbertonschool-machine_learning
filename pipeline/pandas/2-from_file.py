bf55d4b8f603b297de1f70af7b47dd5c5b90423a
Aminekham <lkham0508@gmail.com>
2024-07-30 14:01:04 +0200

    reading a csv file

---
+++ pipeline/pandas/2-from_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+
+"""
+import pandas as pd
+
+
+def from_file(filename, delimiter):
+    """
+
+    """
+    df = pd.read_csv(filename, delimiter = delimiter)
+    return df
