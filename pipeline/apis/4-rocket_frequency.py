7f05e51ba8f121c38bc77fb48de30e4181b9d649
Aminekham <lkham0508@gmail.com>
2024-07-30 13:40:13 +0200

    pycodestyle

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -13,7 +13,8 @@ if __name__ == '__main__':
     launches = requests.get("https://api.spacexdata.com/v4/launches").json()
     for launch in launches:
         if launch["rocket"] not in rockets.keys():
-            rocket_name = requests.get("https://api.spacexdata.com/v4/rockets/{}".format(launch["rocket"])).json()["name"]
+            lk = "https://api.spacexdata.com/v4/rockets/"
+            rocket_name = requests.get("{}{}".format(lk ,launch["rocket"])).json()["name"]
             rockets[launch["rocket"]] = [rocket_name, 1]
         elif launch["rocket"] in rockets.keys():
             rockets[launch["rocket"]][1] += 1
