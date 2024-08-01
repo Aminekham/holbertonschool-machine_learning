7a434cb35a0aa8860ce92e7f01ec31b738c264fa
Aminekham <lkham0508@gmail.com>
2024-08-01 19:15:09 +0200

    fixing

--- pipeline/databases/8-genre_id_by_show.sql
+++ pipeline/databases/8-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- Listing shows with genre
 SELECT CONCAT(tv_shows.title, ' - ', tv_show_genres.genre_id) AS result
-FROM tv_genres
+FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC
