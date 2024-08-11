107883a4657f687cbd1a95d83357eb02b326172b
Aminekham <lkham0508@gmail.com>
2024-08-11 13:44:07 +0200

    quantity decrease

---
+++ pipeline/databases/17-store.sql
@@ -0,0 +1,8 @@
+-- decreasing the quantity of an item when ordered
+CREATE TRIGGER update_item_quantity AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END;
