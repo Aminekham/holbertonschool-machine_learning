054fb33b2614aa9399638c8a14112aafac54e65e
Aminekham <lkham0508@gmail.com>
2024-08-11 18:15:15 +0200

    docs

--- pipeline/databases/30-all.py
+++ pipeline/databases/30-all.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+"""
+the code documentation for looping through a mongodb
+collection and listing them
+"""
 def list_all(mongo_collection):
     """Listing docs in a collection"""
     if mongo_collection is None:
         return []
-
     documents = mongo_collection.find()
     return list(documents)
