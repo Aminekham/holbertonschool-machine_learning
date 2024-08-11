508b98d1a0d8847bc197359d9de89611097e100d
Aminekham <lkham0508@gmail.com>
2024-08-11 14:25:12 +0200

    fixing null handling

--- pipeline/databases/20-average_score.sql
+++ pipeline/databases/20-average_score.sql
@@ -3,7 +3,7 @@ DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     DECLARE avg_score FLOAT;
-    SELECT AVG(score) INTO avg_score
+    SELECT IFNULL(AVG(score), 0) INTO avg_score
     FROM corrections
     WHERE user_id = user_id;
     UPDATE users
