c059c73a7d64f85734518623ab6dc3bb08aae1fb
Aminekham <lkham0508@gmail.com>
2024-12-09 01:29:39 +0100

    working

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+function for element wise operations
+"""
+
+
+def np_elementwise(mat1, mat2):
+    """
+    tuple containing element
+    wise operations all of them on the two matrices
+    """
+    return mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2
