bd2ca435d875906d5d2d58f69b9e9805cabe088d
Aminekham <lkham0508@gmail.com>
2024-12-12 13:42:40 +0100

    two function

--- math/plotting/3-two.py
+++ math/plotting/3-two.py
@@ -2,17 +2,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(0, 21000, 1000)
-r = np.log(0.5)
-t1 = 5730
-t2 = 1600
-y1 = np.exp((r / t1) * x)
-y2 = np.exp((r / t2) * x)
+def two():
+    x = np.arange(0, 21000, 1000)
+    r = np.log(0.5)
+    t1 = 5730
+    t2 = 1600
+    y1 = np.exp((r / t1) * x)
+    y2 = np.exp((r / t2) * x)
+    plt.figure(figsize=(6.4, 4.8))
 
-plt.xlabel('Time (years)')
-plt.ylabel('Fraction Remaining')
-plt.title('Exponential Decay of Radioactive Elements')
-plt.plot(x, y1, '--', color = 'red', label = 'C14')
-plt.plot(x, y2, color = 'green', label = 'Ra-226')
-plt.legend()
-plt.show()
+    plt.xlabel('Time (years)')
+    plt.ylabel('Fraction Remaining')
+    plt.title('Exponential Decay of Radioactive Elements')
+    plt.plot(x, y1, '--', color = 'red', label = 'C14')
+    plt.plot(x, y2, color = 'green', label = 'Ra-226')
+    plt.legend()
+    plt.show()
