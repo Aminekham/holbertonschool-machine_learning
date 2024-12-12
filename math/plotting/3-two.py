5e4dff01eeb0f6201aefc68691eabb24dd3559b6
Aminekham <lkham0508@gmail.com>
2024-12-12 16:17:53 +0100

    exp decay

--- math/plotting/3-two.py
+++ math/plotting/3-two.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
+"""
+Task 3
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def two():
+    """
+    """
     x = np.arange(0, 21000, 1000)
     r = np.log(0.5)
     t1 = 5730
@@ -10,11 +17,12 @@ def two():
     y1 = np.exp((r / t1) * x)
     y2 = np.exp((r / t2) * x)
     plt.figure(figsize=(6.4, 4.8))
-
     plt.xlabel('Time (years)')
     plt.ylabel('Fraction Remaining')
-    plt.title('Exponential Decay of Radioactive Elements')
-    plt.plot(x, y1, '--', color = 'red', label = 'C14')
-    plt.plot(x, y2, color = 'green', label = 'Ra-226')
+    plt.title("Exponential Decay of Radioactive Elements")
+    plt.xlim(0, 20000)
+    plt.ylim(0, 1)
+    plt.plot(x, y1, 'r--', label='C-14')
+    plt.plot(x, y2, 'g-', label='Ra-226')
     plt.legend()
     plt.show()
